c9e5276a2b404082f40161dd8c0675e5f5ef7436
qiuyan1995 <1028511812@qq.com>
2017-07-06 02:45:57 -0500

    Add files via upload

---
+++ 微信小程序/pages/legends/legnds.js
@@ -0,0 +1,116 @@
+// pages/legends/legnds.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    battle: [{
+       img: "../img/2.png",
+       nickname: "1.Braum 布隆 弗雷尔卓德 Support",
+       time:'2017/06/10 12:30:20',
+       battle:"2/12/2 负"
+     },{
+       img: "../img/3.png",
+        nickname: '2.Chogath 科加斯 虚空恐惧 Tank',
+        time:'2017/06/10 12:55:40',
+        battle:"2/10/2 负"
+     },{
+        img: "../img/4.png",
+       nickname: '3.Fiora 菲奥娜 无双剑姬 Fighter',
+       time:'2017/06/10 12:20:50',
+       battle:"2/6/2 负"
+       },{
+        img: "../img/5.png",
+       nickname: '4.Fizz 菲兹 潮汐海灵 Assassin',
+       time:'2017/06/10 12:55:10',
+       battle:"2/7/2 负"
+       },{
+        img: "../img/6.png",
+       nickname: '5.Graves 格雷福斯 法外狂徒 Markman',
+       time:'2017/06/10 12:28:30',
+       battle:"2/8/2 负"
+       },{
+          img: "../img/7.png",
+          nickname: '6.Jhin 烬 戏命师 Markman',
+          time:'2017/06/10 12:48:50',
+          battle:"2/11/2 负"
+       },{
+           img: "../img/8.png",
+           nickname: '7.Lulu 璐璐 仙灵女巫 Support',
+           time:'2017/06/10 12:13:47',
+           battle:"6/10/3 负"
+       },{
+           img: "../img/9.png",
+           nickname: '8.Morgana 莫甘娜 堕落天使 Mage',
+           time:'2017/06/10 11:44:10',
+           battle:"2/5/7 胜"
+       },{
+           img: "../img/10.png",
+           nickname: '9.Nasus 内瑟斯 沙漠死神 Fighter',
+           time:'2017/06/10 11:00:50',
+           battle:"3/8/4 负"
+
+       },{
+           img: "../img/11.png",
+           nickname: '10.Zed 劫 影流之主 Assassin'
+       }]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
